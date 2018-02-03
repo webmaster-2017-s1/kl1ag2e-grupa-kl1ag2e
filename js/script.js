4834b9f65894784d68a83139b9eef3506f50fccf
Marcin Zasuwa <marcinadd@gmail.com>
2018-02-03 16:39:25 +0100

    Dodanie wykrywania platformy nad ktora znajduje sie gracz

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 //Canvas Resolutnion
-var resx=1280;
-var resy=720;
+var resx = 1600;
+var resy = 900;
 
-//Player position
-var px=30;
+//Player position render
+var px = resx / 2;
 var py=40;
 
+//Player position
+
+var pposx = resx / 2;
+
+
 //Player size
-var sx=40;
-var sy=40;
+var sx = 50;
+var sy = 50;
 
 var onair = false;
 
 //Long platform dimensions
-var pnumber = 10;
+var pnumber = 3;
 //[][*]
 //0-X pos
 //1-Y pos
@@ -22,7 +27,7 @@ var pnumber = 10;
 
 
 //Scene position
-var spos = resx;
+var spos = resx / 2;
 
 
 var platforms = [];
@@ -34,13 +39,20 @@ for (i = 0; i < pnumber; i++) {
     // }
 }
 
-platforms[0][0] = 2000;
-platforms[0][1] = 100;
-platforms[0][2] = 80;
+platforms[0][0] = 0;
+platforms[0][1] = resy - 20;
+platforms[0][2] = 20000;
 platforms[0][3] = 20;
 
+platforms[1][0] = 2500;
+platforms[1][1] = 550;
+platforms[1][2] = 250;
+platforms[1][3] = 50;
 
-
+platforms[2][0] = 2600;
+platforms[2][1] = 450;
+platforms[2][2] = 250;
+platforms[2][3] = 50;
 
 
 
@@ -55,26 +67,62 @@ function draw() {
     rect(px,py,sx,sy);
     if(keyIsPressed) keyboardEvent();
     drawObjects();
+    collision();
 }
 
 function keyboardEvent() {
     if(keyIsDown(38)) py-=5;
-    if (keyIsDown(39)) spos += 5;
+    if (keyIsDown(39)) {
+        spos += 5;
+        pposx = spos;
+    }
     if(keyIsDown(40)) py+=5;
-    if (keyIsDown(37)) spos -= 5;
+    if (keyIsDown(37)) {
+        spos -= 5;
+        pposx = spos;
+    }
 
     return false;
 }
 
 function drawObjects() {
+    fill('#0000FF');
+    for (i = 0; i < pnumber; i++) {
+        rect(platforms[i][0] - spos, platforms[i][1], platforms[i][2], platforms[i][3]);
+    }
+    fill('#FFFFFF');
+
+}
+
+function collision() {
+    //Check if  player upward platform
+
+    //Minimum platform posy >=py
+
+    var miny = 9999;
+    var maxid = 0;
 
 
-    rect(platforms[0][0] - spos, platforms[0][1], platforms[0][2], platforms[0][3]);
-
+    for (i = 0; i < pnumber; i++) {
+        //Select platform borders
+        if (px >= platforms[i][0] - pposx && px + sx <= platforms[i][0] - pposx + platforms[i][2]) {
+            // console.log("Player pos: "+i+" platform"); //Uncomment to debug collision
+            if (platforms[i][1] > py) {
+                if (platforms[i][1] < miny) {
+                    miny = platforms[i][1];
+                    maxid = i;
+                }
+            }
+        }
+    }
+    debugcollision(maxid);
 
 }
 
 
-function gravity() {
+function debugcollision(maxid) {
+    fill('red');
+    rect(platforms[maxid][0] - spos, platforms[maxid][1], platforms[maxid][2], platforms[maxid][3]);
+    fill('white');
 
 }
