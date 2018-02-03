6768dd72cde9d89fe08c9bb6a61c4d7855e7d6a3
Marcin Zasuwa <marcinadd@gmail.com>
2018-02-03 09:00:10 +0100

    Dodanie przewijania tla

--- js/script.js
+++ js/script.js
@@ -10,8 +10,42 @@ var py=40;
 var sx=40;
 var sy=40;
 
+var onair = false;
+
+//Long platform dimensions
+var pnumber = 10;
+//[][*]
+//0-X pos
+//1-Y pos
+//2-Width
+//3-Height
+
+
+//Scene position
+var spos = resx;
+
+
+var platforms = [];
+
+for (i = 0; i < pnumber; i++) {
+    platforms[i] = [];
+    // for(j=0;j<4;j++){
+    //     platforms[i][j]=0;
+    // }
+}
+
+platforms[0][0] = 2000;
+platforms[0][1] = 100;
+platforms[0][2] = 80;
+platforms[0][3] = 20;
+
+
+
+
+
 
 function setup() {
+
     createCanvas(resx,resy);
     background(200);
 }
@@ -20,14 +54,27 @@ function draw() {
     background(200);
     rect(px,py,sx,sy);
     if(keyIsPressed) keyboardEvent();
-
+    drawObjects();
 }
 
 function keyboardEvent() {
     if(keyIsDown(38)) py-=5;
-    if(keyIsDown(39)) px+=5;
+    if (keyIsDown(39)) spos += 5;
     if(keyIsDown(40)) py+=5;
-    if(keyIsDown(37)) px-=5;
+    if (keyIsDown(37)) spos -= 5;
 
     return false;
+}
+
+function drawObjects() {
+
+
+    rect(platforms[0][0] - spos, platforms[0][1], platforms[0][2], platforms[0][3]);
+
+
+}
+
+
+function gravity() {
+
 }
