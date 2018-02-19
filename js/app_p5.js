71c06cbf6327261b0138499193cfffffa74aa0eb
Kuba077 <mazurjakub81@gmail.com>
2018-02-19 16:23:20 +0100

    Implementacja wielu plansz. Dodanie drugiej planszy.

--- js/app_p5.js
+++ js/app_p5.js
@@ -38,206 +38,6 @@ var spos = 0;
 //Gravity
 var grav = 7;
 
-//Platform number
-var pnumber = 37;
-//Platforms' dimensions
-//[][*]
-//0-X pos
-//1-Y pos
-//2-Width
-//3-Height
-
-var platforms = [];
-
-for (i = 0; i < pnumber; i++) {
-  platforms[i] = [];
-}
-
-// platforms[0][0] = 0;
-// platforms[0][1] = resy - 20;
-// platforms[0][2] = 20000;
-// platforms[0][3] = 20;
-
-platforms[1][0] = 30;
-platforms[1][1] = 370;
-platforms[1][2] = 320;
-platforms[1][3] = 50;
-
-platforms[2][0] = 1790;
-platforms[2][1] = 210;
-platforms[2][2] = 230;
-platforms[2][3] = 30;
-
-platforms[3][0] = 240;
-platforms[3][1] = 280;
-platforms[3][2] = 270;
-platforms[3][3] = 70;
-
-platforms[4][0] = 650;
-platforms[4][1] = 440;
-platforms[4][2] = 160;
-platforms[4][3] = 70;
-
-platforms[5][0] = 990;
-platforms[5][1] = 390;
-platforms[5][2] = 320;
-platforms[5][3] = 50;
-
-platforms[6][0] = 1200;
-platforms[6][1] = 290;
-platforms[6][2] = 90;
-platforms[6][3] = 100;
-
-platforms[7][0] = 1500;
-platforms[7][1] = 240;
-platforms[7][2] = 520;
-platforms[7][3] = 70;
-
-platforms[8][0] = 2240;
-platforms[8][1] = 410
-platforms[8][2] = 610;
-platforms[8][3] = 70;
-
-platforms[9][0] = 2480;
-platforms[9][1] = 350;
-platforms[9][2] = 100;
-platforms[9][3] = 60;
-
-platforms[10][0] = 2620;
-platforms[10][1] = 350;
-platforms[10][2] = 100;
-platforms[10][3] = 60;
-
-platforms[11][0] = 2990;
-platforms[11][1] = 240;
-platforms[11][2] = 50;
-platforms[11][3] = 180;
-
-platforms[12][0] = 3280;
-platforms[12][1] = 400;
-platforms[12][2] = 100;
-platforms[12][3] = 100;
-
-platforms[13][0] = 2990;
-platforms[13][1] = 240;
-platforms[13][2] = 50;
-platforms[13][3] = 180;
-
-
-platforms[14][0] = 3610;
-platforms[14][1] = 450;
-platforms[14][2] = 320;
-platforms[14][3] = 40;
-
-platforms[15][0] = 3730;
-platforms[15][1] = 330;
-platforms[15][2] = 70;
-platforms[15][3] = 120;
-
-platforms[16][0] = 3870;
-platforms[16][1] = 280;
-platforms[16][2] = 60;
-platforms[16][3] = 170;
-
-platforms[17][0] = 4060;
-platforms[17][1] = 330;
-platforms[17][2] = 10;
-platforms[17][3] = 150;
-
-platforms[18][0] = 4140;
-platforms[18][1] = 430;
-platforms[18][2] = 40;
-platforms[18][3] = 10;
-
-platforms[19][0] = 4350;
-platforms[19][1] = 330;
-platforms[19][2] = 30;
-platforms[19][3] = 150;
-
-platforms[20][0] = 4380;
-platforms[20][1] = 450;
-platforms[20][2] = 70;
-platforms[20][3] = 30;
-
-platforms[21][0] = 4510;
-platforms[21][1] = 580;
-platforms[21][2] = 10;
-platforms[21][3] = 60;
-
-platforms[22][0] = 4680;
-platforms[22][1] = 470;
-platforms[22][2] = 10;
-platforms[22][3] = 90;
-
-platforms[23][0] = 4640;
-platforms[23][1] = 560;
-platforms[23][2] = 90;
-platforms[23][3] = 20;
-
-platforms[24][0] = 4690;
-platforms[24][1] = 340;
-platforms[24][2] = 120;
-platforms[24][3] = 20;
-
-platforms[25][0] = 4840;
-platforms[25][1] = 250;
-platforms[25][2] = 20;
-platforms[25][3] = 90;
-
-platforms[26][0] = 4940;
-platforms[26][1] = 510;
-platforms[26][2] = 60;
-platforms[26][3] = 10;
-
-platforms[27][0] = 5090;
-platforms[27][1] = 410;
-platforms[27][2] = 20;
-platforms[27][3] = 110;
-
-platforms[28][0] = 5110;
-platforms[28][1] = 410;
-platforms[28][2] = 80;
-platforms[28][3] = 20;
-
-platforms[29][0] = 5170;
-platforms[29][1] = 320;
-platforms[29][2] = 20;
-platforms[29][3] = 90;
-
-platforms[30][0] = 5290;
-platforms[30][1] = 470;
-platforms[30][2] = 10;
-platforms[30][3] = 70;
-
-platforms[31][0] = 5370;
-platforms[31][1] = 360;
-platforms[31][2] = 10;
-platforms[31][3] = 190;
-
-platforms[32][0] = 5430;
-platforms[32][1] = 300;
-platforms[32][2] = 40;
-platforms[32][3] = 10;
-
-platforms[33][0] = 5500;
-platforms[33][1] = 360;
-platforms[33][2] = 10;
-platforms[33][3] = 50;
-
-platforms[34][0] = 5520;
-platforms[34][1] = 550;
-platforms[34][2] = 30;
-platforms[34][3] = 10;
-
-platforms[35][0] = 5620;
-platforms[35][1] = 550;
-platforms[35][2] = 10;
-platforms[35][3] = 20;
-
-platforms[36][0] = 5640;
-platforms[36][1] = 410;
-platforms[36][2] = 10;
-platforms[36][3] = 160;
 
 //True if bullets[i][5] in loop is false
 var ready = false;
@@ -362,8 +162,8 @@ function keyTyped() {
 
 function drawObjects() {
   fill('#0000FF');
-  for (i = 0; i < pnumber; i++) {
-    rect(platforms[i][0] - spos, platforms[i][1], platforms[i][2], platforms[i][3]);
+  for (i = 0; i < maxp[stageid]; i++) {
+    rect(platforms[stageid][i][0] - spos, platforms[stageid][i][1], platforms[stageid][i][2], platforms[stageid][i][3]);
   }
   fill('#FFFFFF');
   drawEnemies();
@@ -377,7 +177,7 @@ var miny = 0;
 
 function collision() {
 
-  //TODO Optimise checking to only current rendered platforms
+  //TODO Optimise checking to only current rendered platforms[stageid]
 
   //Maximum player position Y-AXIS
   var maxy = 999999;
@@ -399,23 +199,23 @@ function collision() {
   var xminid = -1;
 
 
-  for (i = 0; i < pnumber; i++) {
+  for (i = 0; i < maxp[stageid]; i++) {
     //***************************
     //Select platform's borders vertical
-    if (px >= platforms[i][0] - pposx - sx && px <= platforms[i][0] - pposx + platforms[i][2]) {
+    if (px >= platforms[stageid][i][0] - pposx - sx && px <= platforms[stageid][i][0] - pposx + platforms[stageid][i][2]) {
       //***TOP***
       // console.log("Player pos: "+i+" platform"); //Uncomment to debug TOP BORDER collision
-      if (platforms[i][1] > py) {
-        if (platforms[i][1] < maxy) {
-          maxy = platforms[i][1];
+      if (platforms[stageid][i][1] > py) {
+        if (platforms[stageid][i][1] < maxy) {
+          maxy = platforms[stageid][i][1];
           maxid = i;
         }
       }
       //***BOTTOM***
       // console.log("Player pos: "+i+" platform"); //Uncomment to debug collision
-      if (platforms[i][1] + platforms[i][3] < py) {
-        if (platforms[i][1] + platforms[i][3] > miny) {
-          miny = platforms[i][1] + platforms[i][3];
+      if (platforms[stageid][i][1] + platforms[stageid][i][3] < py) {
+        if (platforms[stageid][i][1] + platforms[stageid][i][3] > miny) {
+          miny = platforms[stageid][i][1] + platforms[stageid][i][3];
           jheight = min(miny, 150);
           minid = i;
           // console.log("Max Jump Height: "+jheight); //Uncomment to debug Max Jump Height
@@ -424,18 +224,18 @@ function collision() {
     }
     //***************************
     //Select platform's borders horizontal
-    if (py >= platforms[i][1] - sy && py <= platforms[i][1] + platforms[i][3]) {
+    if (py >= platforms[stageid][i][1] - sy && py <= platforms[stageid][i][1] + platforms[stageid][i][3]) {
       //***LEFT***
-      if (platforms[i][0] >= px + sx + pposx) {
-        if (platforms[i][0] < maxx) {
-          maxx = platforms[i][0];
+      if (platforms[stageid][i][0] >= px + sx + pposx) {
+        if (platforms[stageid][i][0] < maxx) {
+          maxx = platforms[stageid][i][0];
           xmaxid = i;
         }
       }
       //***RIGHT***
-      if (platforms[i][0] + platforms[i][2] <= px + pposx) {
-        if (platforms[i][0] > minx) {
-          minx = platforms[i][0] + platforms[i][2];
+      if (platforms[stageid][i][0] + platforms[stageid][i][2] <= px + pposx) {
+        if (platforms[stageid][i][0] > minx) {
+          minx = platforms[stageid][i][0] + platforms[stageid][i][2];
           xminid = i;
         }
       }
@@ -454,13 +254,13 @@ function collision() {
 
 function debugcollision(maxid, minid, xmaxid, xminid) {
   fill('red');
-  rect(platforms[maxid][0] - spos, platforms[maxid][1], platforms[maxid][2], platforms[maxid][3]);
+  rect(platforms[stageid][maxid][0] - spos, platforms[stageid][maxid][1], platforms[stageid][maxid][2], platforms[stageid][maxid][3]);
   fill('green');
-  if (minid !== -1) rect(platforms[minid][0] - spos, platforms[minid][1], platforms[minid][2], platforms[minid][3]);
+  if (minid !== -1) rect(platforms[stageid][minid][0] - spos, platforms[stageid][minid][1], platforms[stageid][minid][2], platforms[stageid][minid][3]);
   fill('yellow');
-  if (xmaxid !== -1) rect(platforms[xmaxid][0] - spos, platforms[xmaxid][1], platforms[xmaxid][2], platforms[xmaxid][3]);
+  if (xmaxid !== -1) rect(platforms[stageid][xmaxid][0] - spos, platforms[stageid][xmaxid][1], platforms[stageid][xmaxid][2], platforms[stageid][xmaxid][3]);
   fill('orange');
-  if (xminid !== -1) rect(platforms[xminid][0] - spos, platforms[xminid][1], platforms[xminid][2], platforms[xminid][3]);
+  if (xminid !== -1) rect(platforms[stageid][xminid][0] - spos, platforms[stageid][xminid][1], platforms[stageid][xminid][2], platforms[stageid][xminid][3]);
   fill('white');
 
 }
