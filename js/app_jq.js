bdde8dd1780b6a5d8742927965a4aa0d74704161
marcins1 <rondusiowy@gmail.com>
2018-04-05 15:53:22 +0200

    Dodanie wersji gry w jQuery, dodanie styli css, uzupelnienie plikow html

--- js/app_jq.js
+++ js/app_jq.js
@@ -1,4 +1,49 @@
-// kod projektu [wpisz nazwę]
+// kod projektu Gra Platformowa
 $(document).ready(function(){
-    ;
+  $("#btn1").click(function() {
+    player.life++;
+  });
+  $("#btn2").click(function() {
+    if (player.life > 1) player.life--;
+  });
+  $("#btn3").click(function() {
+    player.jumpspeed++;
+  });
+  $("#btn4").click(function() {
+    player.jumpspeed--;
+  });
+  $("#btn5").click(function() {
+    if (stageid < 3) {
+      stageid++;
+      game.loadStage(stageid);
+      game.completed = false;
+      game.restartGame();
+    }
+  });
+  $("#btn6").click(function() {
+    if (stageid > 0) {
+      stageid--;
+      game.loadStage(stageid);
+      game.completed = false;
+      game.restartGame();
+    }
+  });
+  $("#btn7").click(function() {
+    player.speedx++;
+  });
+  $("#btn8").click(function() {
+    player.speedx--;
+  });
+  $("#btn9").click(function() {
+    game.grav++;
+  });
+  $("#btn10").click(function() {
+    game.grav--;
+  });
+  $("#btn11").click(function() {
+    player.maxjumpheight = player.maxjumpheight + 5;
+  });
+  $("#btn12").click(function() {
+    player.maxjumpheight = player.maxjumpheight - 5;
+  });
 });
